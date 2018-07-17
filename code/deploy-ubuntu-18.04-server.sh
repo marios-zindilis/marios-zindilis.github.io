d0358c958ce4c6530c26f3bac7f8ca9ecc97557b
Marios Zindilis <marios@zindilis.com>
2018-07-17 20:37:25 +0100

    2018-07-17-20-37-25

--- code/deploy-ubuntu-18.04-server.sh
+++ code/deploy-ubuntu-18.04-server.sh
@@ -31,10 +31,14 @@ apt --yes install fail2ban
 apt --yes install libpam-google-authenticator
 apt --yes install ipcalc
 apt --yes install apache2-mod-security2
+apt --yes install libapache2-mod-wsgi-py3
+apt --yes install sendmail
 
 git config --global user.email "marios@zindilis.com"
 git config --global user.name "Marios Zindilis"
 git config --global push.default simple
+
+echo 'root: marios@zindilis.com' >> /etc/aliases
 
 pip3 install django
 pip3 install flake8
@@ -61,7 +65,7 @@ chmod +x  /home/m/bin/dropbox
 
 if ! grep rss2email /etc/crontab
 then
-    echo '00 */8 * * * root su - m -c "/home/m/Public/Dropbox/Scripts/Python/rss2email271/r2e run"' > /etc/crontab
+    echo '00 */8 * * * root su - m -c "/home/m/Dropbox/Scripts/Python/rss2email271/r2e run"' >> /etc/crontab
 fi
 
 # Drop all IPv6:
@@ -85,11 +89,23 @@ echo 'auth required pam_google_authenticator.so' >> /etc/pam.d/sshd;
 su - marios -c 'google-authenticator'
 service ssh restart
 
+
 rm /etc/apache2/sites-enabled/*
 a2enmod rewrite
+a2enmod ssl
 wget -O /etc/apache2/sites-available/z.zindilis.com.conf https://zindilis.com/code/etc/apache2/sites-available/z.zindilis.com.conf
+wget -O /etc/apache2/sites-available/z.zindilis.com.ssl.conf https://zindilis.com/code/etc/apache2/sites-available/z.zindilis.com.ssl.conf
 a2ensite z.zindilis.com.conf
+a2ensite z.zindilis.com.ssl.conf
+wget -O /etc/apache2/sites-available/x.zindilis.com.conf https://zindilis.com/code/etc/apache2/sites-available/x.zindilis.com.conf
+wget -O /etc/apache2/sites-available/x.zindilis.com.ssl.conf https://zindilis.com/code/etc/apache2/sites-available/x.zindilis.com.ssl.conf
+a2ensite x.zindilis.com.conf
+a2ensite x.zindilis.com.ssl.conf
 cp /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+htpasswd -c /etc/apache2/.htpasswd marios
+
+service apache2 stop
+certbot --standalone --agree-tos -m 'marios@zindilis.com' certonly -d z.zindilis.com
 service apache2 restart
 
 echo 'net.ipv6.conf.all.disable_ipv6 = 1'     >> /etc/sysctl.conf
--- code/sort-hosts.sh
+++ code/sort-hosts.sh
@@ -7,3 +7,8 @@ grep . hosts-BAK | sort | uniq > hosts
 rm hosts-BAK
 
 sudo ./blad
+
+while read -r ip
+do
+    sudo route add -host $ip reject
+done < ip-addresses
